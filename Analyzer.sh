44b8d02316dad148e837b015cfcd91401cc902f4
Diego <m51galaxy@gmail.com>
2019-11-07 13:10:18 +0100

    Small correction: wrong parentheses.

--- Analyzer.sh
+++ Analyzer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 BASEDIR="CC++_Tools"
 
-CCCC="$(BASEDIR)/CCCC/cccc"
-TOKEI="$(BASEDIR)/Tokei/tokei"
-HALSTEAD_TOOL="java -jar $(BASEDIR)/Halstead_Metrics/Halstead-Metrics.jar"
-MI_TOOL="$(BASEDIR)/Maintainability_Index/lizard"
+CCCC="${BASEDIR}/CCCC/cccc"
+TOKEI="${BASEDIR}/Tokei/tokei"
+HALSTEAD_TOOL="java -jar ${BASEDIR}/Halstead_Metrics/Halstead-Metrics.jar"
+MI_TOOL="${BASEDIR}/Maintainability_Index/lizard"
 

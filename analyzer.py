039ada11ba958cc1a5bd1864c1f1e6ac0124024f
Diego <m51galaxy@gmail.com>
2020-01-08 16:29:52 +0100

    Analyzer now returns and writes the json. Results_dir can be specified

--- analyzer.py
+++ analyzer.py
@@ -5,7 +5,6 @@ import datetime
 import os
 import sys
 from exit_codes import ExitCode
-import output_unifier
 import tools
 import json
 
@@ -18,13 +17,17 @@ __DEBUG_F__ = True
 
 #  SE c'è un file .java, TOKEI può analizzarlo... MA meglio che venga DROPPATO!!
 # Dare in base al linguaggio la lista dei tools che si possono usare
-# TODO: OUTPUT Json deve essere standardizzato
 
 
-def analyze(path_to_analyze, tools_path="/home/diego/Development/TESI/2_SoftwareMetrics/"):  # TODO: Delete the def path
+def analyze(path_to_analyze: os.path, tools_path="./CC++_Tools",
+            results_dir="/home/diego/Development/TESI/2_SoftwareMetrics/"):  # TODO: Delete the def path
     if not os.path.exists(path_to_analyze):
         print("ERROR:\tthe given path (" + path_to_analyze + ") does not exists.", file=sys.stderr)
         sys.exit(ExitCode.TARGET_DIR_NOT_FOUND.value)
+    if not os.path.isdir(results_dir):
+        print("ERROR:\tthe results path (" + results_dir + ") does not exists.", file=sys.stderr)
+        sys.exit(ExitCode.TARGET_DIR_NOT_FOUND.value)
+
     t = tools.Tools(tools_path)
     t.check_tools_existence()
 
@@ -35,16 +38,16 @@ def analyze(path_to_analyze, tools_path="/home/diego/Development/TESI/2_Software
         sys.exit(ExitCode.NO_SUPPORTED_FILES_FOUND)
 
     # The output folder in which all the output data will be placed
-    output_dir = datetime.datetime.now().strftime("results_%Y.%m.%d_%H.%M.%S")
-
+    output_name = datetime.datetime.now().strftime("results_%Y.%m.%d_%H.%M.%S")
+    output_dir = os.path.join(results_dir, output_name)
     # In case of an already existing path, add trailing '_'
     while os.path.exists(output_dir):
         output_dir = output_dir + '_'
     os.mkdir(output_dir)
 
     if __DEBUG_F__:
-        print(" DEBUG:\tOK, in output dir: ", output_dir)
-        print(" DEBUG:\tpathToAnalyze: " + path_to_analyze)
+        print("DEBUG:\tOK, in output dir: ", output_dir)
+        print("DEBUG:\tpathToAnalyze: " + path_to_analyze)
         print()
 
     # RUNNING THE EXTERNAL TOOLS
@@ -52,27 +55,32 @@ def analyze(path_to_analyze, tools_path="/home/diego/Development/TESI/2_Software
     raw_outputs = t.get_raw_output()
 
     if __DEBUG_F__:
-        print("DEBUG. RESULTS:")
+        print("\nDEBUG. RESULTS:")
+        print("TOKEI")
         print(raw_outputs["tokei"])
-        print("\n")
+        print("\nCCCC")
         print(raw_outputs["cccc"])
-        print("\n")
+        print("\nMI")
         print(raw_outputs["mi"])
-        print("\n")
+        print("\nHALSTEAD")
         print(raw_outputs["halstead"])
         print("\n")
 
     formatted_outputs = t.get_output()
-    # TODO:↑ Move "list_of_tools(...)" from this point.
+
     json_output = json.dumps(formatted_outputs, sort_keys=True, indent=4)
     # REMEMBER: Json output *must* be printed to be viewed correctly.
 
-    return json_output, raw_outputs
+    with open(os.path.join(output_name, output_name + ".json"), 'w') as output_file:
+        json.dump(formatted_outputs, output_file, sort_keys=True, indent=4)
+
+    return json_output, raw_outputs     # TODO: Delete raw_output
 
 
-def main():
-    if len(sys.argv) != 2 or sys.argv[1] == "--help" or sys.argv[1] == "-help" or sys.argv[1] == "-h":
-        print("USAGE: " + sys.argv[0] + "<path to analyze>", file=sys.stderr)
+def main():     # TODO: Test this!
+    if 2 > len(sys.argv) > 3 or "--help" in sys.argv or "-help" in sys.argv or "-h" in sys.argv:
+        print("USAGE:\t" + sys.argv[0] + "<path to analyze> [<results dir>]", file=sys.stderr)
+        print("\tDefault results dir: current directory.", file=sys.stderr)
         sys.exit(ExitCode.USAGE_HELP.value)
 
-    return analyze(tools_path=sys.argv[0], path_to_analyze=sys.argv[1])
+    analyze(tools_path=sys.argv[0], path_to_analyze=sys.argv[1], results_dir=sys.argv[2])

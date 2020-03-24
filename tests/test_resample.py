4696d9b41acb89dd46a1d8a00a9763f82391096b
Luni-4 <luni-4@hotmail.it>
2020-03-24 12:27:16 +0100

    Add Tokei test for resample

---
+++ tests/test_resample.py
@@ -0,0 +1,24 @@
+"""Test if metrics produced by tools are the same for resample"""
+
+import typing as T
+
+import pytest
+
+from common import compare
+
+
+def test_rust_code_analysis_tokei() -> None:
+    """These tests evaluates if rust-code-analysis and tokei produce
+       the same metrics values.
+    """
+
+    ret_value = compare(
+        "rust-code-analysis",
+        "tokei",
+        ["-g", "-f"],
+        ["SLOC", "LLOC"],
+        "C-C++",
+        "resample.c",
+    )
+
+    assert ret_value == 0

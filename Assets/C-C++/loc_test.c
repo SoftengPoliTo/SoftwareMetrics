d539c1df88e2ffcd33f6e7042c7a94b095f144ca
Luni-4 <luni-4@hotmail.it>
2020-03-25 19:31:31 +0100

    Fix C++ test for LOC metrics

--- Assets/C-C++/loc_test.c
+++ Assets/C-C++/loc_test.c
@@ -1,8 +1,8 @@
 // SLOC = 8, CLOC = 2, LOC = 7, LLOC = 8
-void loc_test {
+void loc_test() {
     a = 6;
     b = 5;
     c = a + b;  d = a - b;
-}
+}                                                                                                                       
 
 // Another comment. Just above a blank line

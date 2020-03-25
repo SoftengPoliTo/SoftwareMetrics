f398f1ec2cf16fdbbf6ca4726bc5e8823225256a
Luni-4 <luni-4@hotmail.it>
2020-03-25 11:54:12 +0100

    Add Rust tests for Tokei

--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -7,9 +7,9 @@ import pytest
 from common import compare
 
 
-def test_rust_code_analysis_tokei() -> None:
+def test_rust_code_analysis_tokei_c() -> None:
     """These tests evaluates if rust-code-analysis and tokei produce
-       the same metrics values.
+       the same metrics values for C language.
     """
 
     ret_value = compare(
@@ -19,6 +19,22 @@ def test_rust_code_analysis_tokei() -> None:
         ["SLOC", "LLOC", "CLOC"],
         "C-C++",
         "bubble_sort.c",
+    )
+
+    assert ret_value == 0
+
+def test_rust_code_analysis_tokei_Rust() -> None:
+    """These tests evaluates if rust-code-analysis and tokei produce
+       the same metrics values for Rust language.
+    """
+
+    ret_value = compare(
+        "rust-code-analysis",
+        "tokei",
+        ["-g", "-f"],
+        ["SLOC", "LLOC", "CLOC"],
+        "Rust",
+        "bubble_sort.rs",
     )
 
     assert ret_value == 0

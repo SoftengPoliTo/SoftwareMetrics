52a83b3189d1ca109212e6a895711d82132ab795
Luni-4 <luni-4@hotmail.it>
2020-03-25 19:31:50 +0100

    Add Rust test for LOC metrics

---
+++ Assets/Rust/loc_test.rs
@@ -0,0 +1,8 @@
+// SLOC = 8, CLOC = 2, LOC = 7, LLOC = 8
+fn loc_test() {
+    let a = 6;
+    let b = 5;
+    let mut c = a + b;  let mut d = a - b;
+}                                                                                                                       
+
+// Another comment. Just above a blank line

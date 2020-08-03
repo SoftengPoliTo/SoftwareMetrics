3e5025ac4e3d6c92bec06c50c5276f2e9f435f08
Luni-4 <luni-4@hotmail.it>
2020-08-03 12:29:29 +0200

    Document output unifier

--- output_unifier.py
+++ output_unifier.py
@@ -1,7 +1,9 @@
-from cccc import helper_cccc, helper_test_cccc, standardizer_cccc
+""" Unifies the metrics produced by a tool on different files in a single
+json file, then uses the retrieved data to compute the global metrics """
+
+from cccc import helper_test_cccc, standardizer_cccc
 from exit_codes import log_warn
 from halstead import (
-    helper_halstead,
     helper_test_halstead,
     standardizer_halstead,
 )
@@ -14,13 +16,14 @@ from tokei import helper_test_tokei, standardizer_tokei
 
 
 def unifier(tool_manager, files_to_analyze, one_json_per_tool):
-    # Standardize the output of each tool and
-    # compute the final global metrics
     outputs = {}
     for tool in tool_manager.get_enabled_tools():
+        # Standardize the output of each tool
         standardized_output = globals()[
             "standardizer_" + tool.replace("-", "_")
         ](tool_manager.get_tool_output(tool))
+        # Compute the final global metrics starting from the standardized
+        # output of a tool
         if tool_manager.get_tool_output(tool):
             outputs[tool] = globals()["helper_test_" + tool.replace("-", "_")](
                 standardized_output
